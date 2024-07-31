a8ba45469f8ee5048eaa07e4b18aa399e0de919f
Андрей Сидоренко <andrew@MacBook-Pro---Andrew.local>
2024-07-31 23:09:12 +0200

    Log In and Sign Up screens

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import {useNavigation} from '../navigation/types/navigation';
+import {useState} from 'react';
 
 export default function LoginScreen() {
   const navigation = useNavigation<'LogIn'>();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
 
   return (
     <View style={styles.container}>
@@ -12,14 +23,49 @@ export default function LoginScreen() {
         onPress={() => navigation.goBack()}>
         <Text style={styles.backButtonText}>Back</Text>
       </TouchableOpacity>
-      <Text style={styles.title}>Login Screen</Text>
-      {/* Add your login form here */}
-      <Button
-        title="Login"
+      <Image source={require('../../assets/Mulo.png')} style={styles.image} />
+      <Image
+        source={require('../../assets/morkovka.png')}
+        style={styles.icon}
+      />
+      <Text style={styles.title}>Log In</Text>
+      <Text style={styles.subtitle}>Текст...</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={secureTextEntry}
+          autoCapitalize="none"
+        />
+        <TouchableOpacity onPress={() => setSecureTextEntry(!secureTextEntry)}>
+          <Text style={styles.eyeIcon}>{secureTextEntry ? '👁' : '🙈'}</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => {
           /* Handle login */
-        }}
-      />
+        }}>
+        <Text style={styles.buttonText}>Log In</Text>
+      </TouchableOpacity>
+      <Text style={styles.switchText}>
+        Don't have an account?{' '}
+        <Text
+          style={styles.switchLink}
+          onPress={() => navigation.navigate('SignUp')}>
+          Sign Up
+        </Text>
+      </Text>
     </View>
   );
 }
@@ -29,11 +75,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
   },
   backButton: {
     position: 'absolute',
@@ -47,5 +88,63 @@ const styles = StyleSheet.create({
   backButtonText: {
     color: 'white',
     fontSize: 16,
+  },
+  image: {
+    width: '100%',
+    height: 200, // Adjust as needed
+    resizeMode: 'contain',
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    position: 'absolute',
+    top: 150, // Adjust as needed
+    left: 20, // Adjust as needed
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+    textAlign: 'left',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#888',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  eyeIcon: {
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: '#53B175',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  switchText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 16,
+  },
+  switchLink: {
+    color: '#53B175',
+    fontWeight: 'bold',
   },
 });
--- src/screens/RegistrationScreen.tsx
+++ src/screens/RegistrationScreen.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import {useNavigation} from '../navigation/types/navigation';
 
 export default function RegistrationScreen() {
   const navigation = useNavigation<'SignUp'>();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
 
   return (
     <View style={styles.container}>
@@ -12,14 +23,55 @@ export default function RegistrationScreen() {
         onPress={() => navigation.goBack()}>
         <Text style={styles.backButtonText}>Back</Text>
       </TouchableOpacity>
-      <Text style={styles.title}>Registration Screen</Text>
-      {/* Add your registration form here */}
-      <Button
-        title="Sign Up"
+      <Image source={require('../../assets/Mulo.png')} style={styles.image} />
+      <Image
+        source={require('../../assets/morkovka.png')}
+        style={styles.icon}
+      />
+      <Text style={styles.title}>Sign Up</Text>
+      <Text style={styles.subtitle}>Текст...</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={secureTextEntry}
+          autoCapitalize="none"
+        />
+        <TouchableOpacity onPress={() => setSecureTextEntry(!secureTextEntry)}>
+          <Text style={styles.eyeIcon}>{secureTextEntry ? '👁' : '🙈'}</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => {
           /* Handle sign up */
-        }}
-      />
+        }}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+      <Text style={styles.switchText}>
+        Already have an account?{' '}
+        <Text
+          style={styles.switchLink}
+          onPress={() => navigation.navigate('LogIn')}>
+          Log In
+        </Text>
+      </Text>
     </View>
   );
 }
@@ -29,11 +81,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
   },
   backButton: {
     position: 'absolute',
@@ -48,5 +95,63 @@ const styles = StyleSheet.create({
     fontFamily: 'Gilroy-Medium',
     color: 'white',
     fontSize: 16,
+  },
+  image: {
+    width: '100%',
+    height: 200, // Adjust as needed
+    resizeMode: 'contain',
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    position: 'absolute',
+    top: 150, // Adjust as needed
+    left: 20, // Adjust as needed
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+    textAlign: 'left',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#888',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  passwordContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  eyeIcon: {
+    marginLeft: 10,
+  },
+  button: {
+    backgroundColor: '#53B175',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  switchText: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 16,
+  },
+  switchLink: {
+    color: '#53B175',
+    fontWeight: 'bold',
   },
 });
